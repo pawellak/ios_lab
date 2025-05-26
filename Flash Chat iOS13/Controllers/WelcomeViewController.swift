da6d22234d4f398be6c33f9219469f283323e23d
Paweł Łąk <pawellak@gmail.com>
2025-05-26 15:21:56 +0200

    cocoapods && CLTypingLabel

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,36 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    
-    
-    
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+//        
+//        titleLabel.charInterval = 0.0 //optional, default is 0.1
+        self.titleLabel.text = "⚡️AlashChat"
         
+          
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { (timer) in
+            self.titleLabel.text = "NewTest"
+                    }
         
-       
+      
+        
+//        titleLabel.text = ""
+//                
+//        let titleText = "⚡️FlashChat"
+//        var charIndex = 0.0
+//        
+//        for letter in titleText
+//        {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex = charIndex + 1.0
+//        }
     }
-    
-
 }
