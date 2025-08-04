41e9923e74116711effc17617c1d4ed3edae34db
Paweł Łąk <pawellak@gmail.com>
2025-08-04 22:15:45 +0200

    add old project: I-am-Rich

---
+++ I-am-Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Paweł Łąk on 13/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
